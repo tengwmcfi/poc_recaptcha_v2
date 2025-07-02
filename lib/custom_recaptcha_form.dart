98c8c26d5420617dab34dc93b6611f18b7a3d1db
Irwan Chandra <ic@irwancheung.com>
2025-07-02 16:44:04 +0700

    add show log function

--- lib/custom_recaptcha_form.dart
+++ lib/custom_recaptcha_form.dart
@@ -1,4 +1,4 @@
-import 'dart:developer';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -14,10 +14,11 @@ class CustomRecaptchaForm extends StatefulWidget {
 class _CustomRecaptchaFormState extends State<CustomRecaptchaForm> {
   static const String _siteKey = '6LesrGQrAAAAAI0Ci_d-YE7cYaVlGzMZ6L2JEGIi';
   static const String _secretKey = '6LesrGQrAAAAADMCVBtNjDSQzVYSUVeNmkpgiqvB';
-  static const String _recaptchaUrl =
-      'https://cfi-recaptcha-v2.netlify.app?sitekey=$_siteKey';
+  static const String _recaptchaUrl = 'https://cfi-recaptcha-v2.netlify.app';
   static const String _verifyUrl =
       'https://www.google.com/recaptcha/api/siteverify';
+
+  final _log = ValueNotifier<String>('');
 
   bool _canSubmit = false;
 
@@ -26,23 +27,24 @@ class _CustomRecaptchaFormState extends State<CustomRecaptchaForm> {
   @override
   void initState() {
     super.initState();
+    _addLog('Load recaptcha in Webview:\nURL: $_recaptchaUrl');
     _webViewController = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(Colors.transparent)
       ..addJavaScriptChannel(
         'RecaptchaHandler',
         onMessageReceived: (message) {
-          log('Received token from webview: ${message.message}');
           final token = message.message;
+          _addLog('Captured token: ${token.substring(0, 5)}... from Webview');
           _validateToken(token);
         },
       )
-      ..loadRequest(Uri.parse(_recaptchaUrl));
+      ..loadRequest(Uri.parse(_recaptchaUrl + '?sitekey=$_siteKey'));
   }
 
   // backend verification of the reCAPTCHA token
   Future<void> _validateToken(String token) async {
-    log('Validating token');
+    _addLog('Validate token in: $_verifyUrl');
     try {
       final response = await http.post(Uri.parse(_verifyUrl), body: {
         'secret': _secretKey,
@@ -52,16 +54,23 @@ class _CustomRecaptchaFormState extends State<CustomRecaptchaForm> {
       if (response.statusCode == 200 &&
           response.body.contains('"success": true')) {
         setState(() {
-          log('Token verified successfully');
-          _canSubmit = true;
+          _addLog('Result: ${response.body}');
+          _addLog('Token is verified by Google');
+          final data = jsonDecode(response.body) as Map<String, dynamic>;
+          _addLog('Checking hostname: ${data["hostname"]}');
+
+          if (_recaptchaUrl.contains(data['hostname'])) {
+            _addLog('Token hostname verified. Enable Submit button');
+            _canSubmit = true;
+          } else {
+            _addLog('Token verification failed: Invalid hostname');
+          }
         });
       } else {
-        log('Token verification failed: ${response.body}');
-        _showSnackbar('Verification failed: ${response.body}');
+        _addLog('Token verification failed: ${response.body}');
       }
     } catch (e) {
-      log('Error verifying token: $e');
-      _showSnackbar('Error verifying token: $e');
+      _addLog('Error verifying token: $e');
     }
   }
 
@@ -70,9 +79,16 @@ class _CustomRecaptchaFormState extends State<CustomRecaptchaForm> {
         .showSnackBar(SnackBar(content: Text(message)));
   }
 
+  void _addLog(String log) {
+    _log.value = _log.value.isEmpty ? '- $log' : '${_log.value}\n- $log';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Custom Recaptcha Form'),
+      ),
       resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Container(
@@ -80,15 +96,28 @@ class _CustomRecaptchaFormState extends State<CustomRecaptchaForm> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: ElevatedButton(
-                  onPressed: _canSubmit
-                      ? () => _showSnackbar('Data submitted successfully')
-                      : null,
-                  child: const Text('Submit'),
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: _canSubmit
+                        ? () => _showSnackbar('Data submitted successfully')
+                        : null,
+                    child: const Text('Submit'),
+                  ),
                 ),
+              ),
+              ValueListenableBuilder(
+                valueListenable: _log,
+                builder: (context, log, _) {
+                  return Text(
+                    log,
+                    textAlign: TextAlign.start,
+                    overflow: TextOverflow.visible,
+                  );
+                },
               ),
               Expanded(
                 child: WebViewWidget(
